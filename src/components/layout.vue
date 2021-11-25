66a7cd5799c8722831d31b89fd9b4ec4cd071c76
lujiesheng <2923280915@qq.com>
2021-11-25 18:08:12 +0800

    动态绑定侧边栏菜单区域

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,38 +1,69 @@
 <template>
   <el-container class="home-container">
-    <!-- 左侧导航 -->
+    <!-- 左侧导航 start -->
     <el-aside class="home-aside">
-      <!-- 侧边栏菜单区域 -->
+      <!-- 侧边栏菜单区域 start -->
       <el-menu class="home-menu" :default-active="this.$route.path" background-color="rgb(48, 65, 86)" text-color="#fff" router>
-
         <template v-for="m in this.menuList">
-          <el-menu-item :index="m.MenuUrl" :key="m.MenuUrl">
-            <i :class="m.MenuIcon"></i>
-            <span slot="title">{{m.MenuName}}</span>
-          </el-menu-item>
-        </template>
-
-        <!-- <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-s-home"></i>
-            <span>一级菜单</span>
+          <!-- 一级菜单 start -->
+          <template v-if="m.SubMenuList.length>0">
+            <el-submenu :index="m.MenuUrl" :key="m.MenuUrl">
+              <template slot="title">
+                <i :class="m.MenuIcon"></i>
+                <span>{{m.MenuName}}</span>
+              </template>
+              <!-- 二级菜单 start -->
+              <template v-if="m.SubMenuList.length>0">
+                <template v-for="mm in m.SubMenuList">
+                  <el-submenu :index="mm.MenuUrl" :key="mm.MenuUrl">
+                    <template slot="title">
+                      <i :class="mm.MenuIcon"></i>
+                      <span>{{mm.MenuName}}</span>
+                    </template>
+                    <!-- 三级菜单 start -->
+                    <template v-if="mm.SubMenuList.length>0">
+                      <template v-for="mmm in mm.SubMenuList">
+                        <el-submenu :index="mmm.MenuUrl" :key="mmm.MenuUrl">
+                          <template slot="title">
+                            <i :class="mmm.MenuIcon"></i>
+                            <span>{{mmm.MenuName}}</span>
+                          </template>
+                        </el-submenu>
+                      </template>
+                    </template>
+                    <template v-else>
+                      <el-menu-item :index="mm.MenuUrl" :key="mm.MenuUrl">
+                        <i :class="mm.MenuIcon"></i>
+                        <span slot="title">{{mm.MenuName}}</span>
+                      </el-menu-item>
+                    </template>
+                    <!-- 三级菜单 end -->
+                  </el-submenu>
+                </template>
+              </template>
+              <template v-else>
+                <el-menu-item :index="m.MenuUrl" :key="m.MenuUrl">
+                  <i :class="m.MenuIcon"></i>
+                  <span slot="title">{{m.MenuName}}</span>
+                </el-menu-item>
+              </template>
+              <!-- 二级菜单 end -->
+            </el-submenu>
           </template>
-          <el-submenu index="2-1">
-            <template slot="title">
-              <i class="el-icon-s-home"></i>
-              <span>二级菜单</span>
-            </template>
-            <el-menu-item index="2-1-1">
-              <i class="el-icon-s-home"></i>
-              <span slot="title">三级菜单</span>
+          <template v-else>
+            <el-menu-item :index="m.MenuUrl" :key="m.MenuUrl">
+              <i :class="m.MenuIcon"></i>
+              <span slot="title">{{m.MenuName}}</span>
             </el-menu-item>
-          </el-submenu>
-        </el-submenu> -->
-
+          </template>
+          <!-- 一级菜单 end -->
+        </template>
       </el-menu>
+      <!-- 侧边栏菜单区域 end -->
     </el-aside>
+    <!-- 左侧导航 end -->
     <el-container>
-      <!-- 头部 -->
+      <!-- 头部 start -->
       <el-header class="home-header">
         <span>博客后台管理系统</span>
         <el-dropdown style="height:100%">
@@ -47,10 +78,12 @@
         <!--  -->
         <!-- 面包屑 -->
       </el-header>
-      <!-- 主体部分 -->
+      <!-- 头部 end -->
+      <!-- 主体部分 start -->
       <el-main class="home-main">
         <router-view />
       </el-main>
+      <!-- 主体部分 end -->
     </el-container>
   </el-container>
 </template>
@@ -84,7 +117,7 @@ export default {
     getMenuTree() {
       this.$api({
         method: 'get',
-        url: '/Menu/GetList',
+        url: '/Menu/GetMenuTree',
         data: {}
       }).then(res => {
         if (res != null && res.data != null) {
@@ -92,7 +125,7 @@ export default {
           if (status == 200) {
             this.$msg.success('获取菜单列表成功')
             this.menuList = data
-            console.log(this.menuList)
+            //console.log(this.menuList)
           }
           else {
             this.$msg.error(msg)
