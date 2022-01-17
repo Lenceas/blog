4efa4316cc78301ee0401b3a8f8f6637207c04e6
lujiesheng <2923280915@qq.com>
2022-01-17 11:52:42 +0800

    首页底部链接文字

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,7 +5,9 @@
       <!-- 主体部分 -->
       <el-main class="home-cmain"></el-main>
       <!-- 底部 -->
-      <el-footer style="30px;">
+      <el-footer style="60px;">
+        <span class="hfbq">Copyright © 2022 Lenceas.Blog - 卢杰晟的个人博客</span><br/>
+        <span class="hfbq">Powered by .NET 6 & Vue 2.x on Docker & CentOS</span><br/>
         <el-link :underline='false' type="primary" href="http://beian.miit.gov.cn/" target="_blank">湘ICP备2021003806号-1</el-link>
       </el-footer>
     </el-main>
